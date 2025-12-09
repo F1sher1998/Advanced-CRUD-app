dab61a4b28be2930843ae404ecec3e330176afb9
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-09 14:08:51 +0100

    Commit 5) Main file
    Fixed and reconfigured a main file AKA localhost server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './config/db.js';
+import userRouter from './routes/userRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -20,9 +23,17 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Routes
-
+app.use('/api', userRouter);
 
 // Error handling
+app.use(errorHandler);
+
+// Database connection test
+pool.connect()
+    .then(async client => {
+        const query = 'SELECT NOW()::date AS "theTime"';
+        const result = await client.query(query)
+        console.log(result.rows[0].theTime)})
 
 
 // Start the server
