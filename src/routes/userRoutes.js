7f1f1fb208ca5e9d9d891b89f016273b76394df5
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-09 14:12:48 +0100

    Commit 10) Routes
    Configured all user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import Router from 'express';
+import * as userController from '../controllers/userController.js';
+
+
+const router = Router();
+
+router.get('/users', userController.usersGetAll);
+router.get('/user/:id', userController.userGetById);
+router.post('/user', userController.userCreate);
+router.put('/user/:id', userController.userUpdate);
+router.delete('/user/:id', userController.userDelete);
+
+export default router;
