8d369ccfba90054e8933a055c9406fa59e7e1820
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-10 15:31:03 +0100

    Commit 16) FIX
    Added refresh token column to the user table, updated password column to hashedpassword and removed comments as they were causing syntax errors

--- user-table.sql
+++ user-table.sql
@@ -2,9 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    hashedpassword VARCHAR(100) NOT NULL,
+    refresh_token VARCHAR(500),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-### Add as needed additional tables below ###
